33031b0a4ada7e53700543a0aec8de224399b793
GreatV <vincent2015a@aliyun.com>
2017-05-14 21:21:50 +0800

    change some files

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# CloudMusic-Crawler
-网易云音乐爬虫，数据可视化。
+﻿# 网易云音乐爬虫
+---
+
+## Introduction
+
+看见有人写了一篇[我用Python分析了42万字的歌词，为了搞清楚民谣歌手们在唱些什么](https://ask.hellobi.com/blog/spuerwdk/6336)，觉得挺好玩的，于是就想自己也实现一下。于是本作品就诞生了。
+
+## 爬虫
+
+爬虫部分主要是调用已有的 API。这部分的工作可以参考[NetEase-MusicBox](https://github.com/darknessomi/musicbox)，该作品作者实现了网易云音乐的命令行版，我用了一下还不错。主要参考了该作者的[api.py](https://github.com/darknessomi/musicbox/blob/master/NEMbox/api.py)部分。
+
+![Screenshot3.png](http://bucket0zero-1253582002.file.myqcloud.com/mdimg/Screenshot3.png)
+
+## 文件处理
+
+该部分主要的工作是将所有歌词写入一个文件，同时每个作者的所有歌词也放入一个文件，以备后面的分析只用。
+
+![Screenshot4.png](http://bucket0zero-1253582002.file.myqcloud.com/mdimg/Screenshot4.png)
+
+本次获取的歌词大概 26000 行。
+
+## 文本分析
+
+分词用的是[“结巴”中文分词](https://github.com/fxsjy/jieba)。
+
+我首先选取了一位歌手作为代表分析了一下词频，如下所示：
+
+![shisanfigure_2.png](http://bucket0zero-1253582002.file.myqcloud.com/mdimg/shisanfigure_2.png)
+
+![figure_bar01.png](http://bucket0zero-1253582002.file.myqcloud.com/mdimg/figure_bar01.png)
+
+![figure_pie01.png](http://bucket0zero-1253582002.file.myqcloud.com/mdimg/figure_pie01.png)
+
+做了一个词云：
+
+![shisanfigure_1.png](http://bucket0zero-1253582002.file.myqcloud.com/mdimg/shisanfigure_1.png)
+
+然后。把所有的歌词都分析了一下，得到了如下饼状图：
+
+![fm3.png](http://bucket0zero-1253582002.file.myqcloud.com/mdimg/fm3.png)
+
+还做了一个词云，如下所示：
+
+![fm0.png](http://bucket0zero-1253582002.file.myqcloud.com/mdimg/fm0.png)
+
+## 接下来的工作
+
+- 情绪分析
+- 云音乐的评论很精彩，可以做一下评论，看看有什么发现
+- 饼状图太丑，想换一个
+
+
+
--- api_simple.py
+++ api_simple.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # @Author: GreatV
 # @Date: 2017-04-16
-# 参考 https://github.com/darknessomi/musicbox/
+# 参考 https://github.com/darknessomi/musicbox
 
 import requests
 
 headers = {
-    'Accept': '*/*',
+	'Accept': '*/*',
     'Accept-Encoding': 'gzip,deflate,sdch',
     'Accept-Language': 'zh-CN,zh;q=0.8',
     'Connection': 'keep-alive',
@@ -18,51 +18,45 @@ headers = {
 
 cookies = {'appver': '1.5.2'}
 
-
 def get_artist_music(artist_id):
-    url = 'http://music.163.com/api/artist/{}'.format(artist_id)
+	url = 'http://music.163.com/api/artist/{}'.format(artist_id)
 
-    try:
-        r = requests.get(url, headers=headers, cookies=cookies)
-        return r.json()['hotSongs']
-    except requests.exceptions.RequestException as e:
-        print e
-        return []
+	try:
+		r = requests.get(url, headers = headers, cookies = cookies)
+		return r.json()['hotSongs']
+	except requests.exceptions.RequestException as e:
+		print e
+		return []
 
 
 def get_music_lyric(song_id):
-    url = 'http://music.163.com/api/song/lyric?os=pc&id={}&lv=-1&kv=-1&tv=-1'.format(
-        song_id)
+	url = 'http://music.163.com/api/song/lyric?os=pc&id={}&lv=-1&kv=-1&tv=-1'.format(song_id)
 
-    try:
-        r = requests.get(url, headers=headers, cookies=cookies)
-        if 'lrc' in r.json() and 'lyric' in r.json()['lrc'] and r.json()[
-                'lrc']['lyric'] is not None:
-            return r.json()['lrc']['lyric']
-        else:
-            return []
-    except requests.exceptions.RequestException as e:
-        print e
-        return []
-
+	try:
+		r = requests.get(url, headers = headers, cookies = cookies)
+		if 'lrc' in r.json() and 'lyric' in r.json()['lrc'] and r.json()['lrc']['lyric'] is not None:
+			return r.json()['lrc']['lyric']
+		else:
+			return []
+	except requests.exceptions.RequestException as e:
+		print e
+		return []
 
 def get_music_comments(song_id, offset=0, total='fasle', limit=100):
-    url = 'http://music.163.com/api/v1/resource/comments/R_SO_4_{}/'.format(
-        song_id)
-    payload = {
-        'rid': 'R_SO_4_{}'.format(song_id),
-        'offset': offset,
-        'total': total,
-        'limit': limit
-    }
+	url = 'http://music.163.com/api/v1/resource/comments/R_SO_4_{}/'.format(song_id)
+	payload = {
+		'rid': 'R_SO_4_{}'.format(song_id),
+		'offset': offset,
+		'total': total,
+		'limit': limit
+	}
 
-    try:
-        r = requests.get(url, headers=headers, cookies=cookies, params=payload)
-        return r.json()
-    except requests.exceptions.RequestException as e:
-        print e
-        return []
-
+	try:
+		r = requests.get(url, headers = headers, cookies = cookies, params = payload)
+		return r.json()
+	except requests.exceptions.RequestException as e:
+		print e
+		return []
 
 if __name__ == '__main__':
-    pass
+	pass
