64a6ce35e1c45a523b9a930b67069f9c5bf88d31
greatx <vincent2015a@aliyun.com>
2017-08-17 11:25:40 +0800

    refactoring code

--- api_simple.py
+++ api_simple.py
@@ -13,10 +13,23 @@ headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Host': 'music.163.com',
     'Referer': 'http://music.163.com/search/',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
 }
 
 cookies = {'appver': '1.5.2'}
+
+
+def get_playlist_detail(playlist_id):
+	url = 'http://music.163.com/api/playlist/detail?id={}'.format(playlist_id)
+
+	try:
+		r = requests.get(url, headers = headers, cookies = cookies)
+		return r.json()['result']['tracks']
+	except requests.exceptions.RequestException as e:
+		print(e)
+		return []
+
 
 def get_artist_music(artist_id):
 	url = 'http://music.163.com/api/artist/{}'.format(artist_id)
@@ -25,7 +38,7 @@ def get_artist_music(artist_id):
 		r = requests.get(url, headers = headers, cookies = cookies)
 		return r.json()['hotSongs']
 	except requests.exceptions.RequestException as e:
-		print e
+		print(e)
 		return []
 
 
@@ -39,8 +52,9 @@ def get_music_lyric(song_id):
 		else:
 			return []
 	except requests.exceptions.RequestException as e:
-		print e
+		print(e)
 		return []
+
 
 def get_music_comments(song_id, offset=0, total='fasle', limit=100):
 	url = 'http://music.163.com/api/v1/resource/comments/R_SO_4_{}/'.format(song_id)
@@ -55,8 +69,9 @@ def get_music_comments(song_id, offset=0, total='fasle', limit=100):
 		r = requests.get(url, headers = headers, cookies = cookies, params = payload)
 		return r.json()
 	except requests.exceptions.RequestException as e:
-		print e
+		print(e)
 		return []
+
 
 if __name__ == '__main__':
 	pass
